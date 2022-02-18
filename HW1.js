b132e6f4326171a37ca6b45d1a506b53ea6313b9
B <brendacassita@Brendas-MacBookPro.local>
2022-02-17 18:48:53 -0700

    thursday update

--- HW1.js
+++ HW1.js
@@ -62,16 +62,19 @@
 // 6. *****DONE*****
 //what will this log?
 //console.log('is odd')
+// console.log('----------')
 
 // 7. *****DONE*****
 //what is the value of isOdd(2) 
 // 0, true
+// console.log('----------')
 
 // 8. ******DONE*****
 //Fix the isOdd function to make this work
 // const isOdd = (num) =>{
 //     return num % 2 === 1
 // }
+
 
 // if(isOdd(2)){
 //     console.log('is Odd')
@@ -115,9 +118,11 @@
 //    return sum
 // }
 // console.log(getSum([0,10,20]));
+// console.log('----------')
 
 
 // 3. Write a function that returns the number of vowels in string
+// console.log('returns number of vowels in string')
 // function vowelCount(string){
 //   let count = 0;
 //   const vowels = ['a', 'e', 'i','o', 'u']
@@ -130,9 +135,11 @@
 //    return count
 // }
 // console.log(vowelCount('Hi there'));
+// console.log('----------')
 
 
 // 4. Write a function that takes a math operator, as a string, ('*','/','+','-') and two numbers and returns the result
+// console.log('returns math opp as string')
 // function math(opp, num1, num2){
 //   if (opp ==='+'){
 //     return num1 + num2
@@ -148,11 +155,13 @@
 // console.log(math('-', 11, 10))
 // console.log(math('*', 1, 10))
 // console.log(math('/', 100, 10))
+// console.log('----------')
 
 
 // 5. *****DONE*****
 // Write a function that takes an array of strings and a string and returns true or false 
 // if the string is in the array.
+// console.log('returns array of strings and string and returns true or false if string is in array')
 // function doesExist(arrayOfStrings, string){
 //   for (let i = 0; i <arrayOfStrings.length; i++){
 //     if(arrayOfStrings[i] === string){
@@ -162,10 +171,12 @@
 //   return false;
 // }
 // console.log(doesExist(['orange','apple', 'pear'], 'grape'))
+// console.log('----------')
 
 
 // 6. ******DONE*****
 // write a function that takes a person object {firstname: 'joe', lastname: 'shmoe', age: 50} and returns a formatted string "joe shmoe is 50 years old"
+// console.log('returns person object as string')
 // let joe = {firstname: 'joe', lastname: 'shmoe', age: 50} //create person
 
 // function aboutPerson(person){
@@ -174,10 +185,12 @@
 // }
 
 // console.log(aboutPerson(joe)) //pass person
+// console.log('----------')
 
 
 // 7. *****DONE*****
 // write a function that takes an array that removes the first and last items and return that new array
+// console.log('removes first and last item and returns new array')
 // let array = ['first', 'second', 'third', 'last']
 // // array.shift() 
 // // array.pop()
@@ -191,42 +204,48 @@
 //   }
 //   console.log(removesFirstAndLast(['first', 'second', 'third', 'last']))
 //   console.log(removesFirstAndLast(['1', '2', '3', '4']))
+// console.log('----------')
 
 
 // 8. *****DONE*****
 // write a function that swaps the last and first items of an array 
-// console.log('----------');
+// console.log('swaps first and last')
 // console.log('swap 1st & last array items');
 // let arraySwap = ['first', 'second', 'third', 'last'];
 //  [arraySwap[0], arraySwap[arraySwap.length-1]] = [arraySwap[arraySwap.length-1], arraySwap[0]]
 //  console.log(arraySwap)
+// console.log('----------')
 
 
 // 9. write a function that takes a array and a second argument and returns the index in the array of the second argument
+// console.log('returns index of array')
 // const arrayOfFruits = ['apple', 'orange', 'grape']
 
 // function findIndexOfItemInArray(array, itemInArray){
 //   return array.indexOf(itemInArray)
 // }
-
 // console.log(findIndexOfItemInArray(arrayOfFruits,'grape'))
+// console.log('----------')
 
 
 // 10. *****DONE*****
 // write a function that takes a number and uses a ternary to return if the number is odd or even.
+// // console.log('returns odd or even as ternary')
 // function isOddOrEven(num){
 //   num % 2 === 0 ? console.log(num, 'is even') : console.log(num, 'is odd')
 // }
 
 // isOddOrEven(1)
 // isOddOrEven(2)
-
+// console.log('----------')
 
 // 11. *****DONE*****
 //Write a function that prints all  numbers from 0 – 10
+// console.log('prints all numbers 0-10')
 // function printToTen(){
 //   for(let i = 0; i<=10; i++){
 //     console.log(i);
 //   }
 // }
 // printToTen();
+// console.log('----------')
